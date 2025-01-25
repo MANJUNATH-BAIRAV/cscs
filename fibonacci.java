633973f8fbb74ffe38e961bf4aa40239b68c8533
MANJUNATH-BAIRAV <manjunathbairav1@gmail.com>
2025-01-25 14:12:07 +0530

    help

---
+++ fibonacci.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class fibonacci
+{
+ public static void main(String[] args)
+ {
+ Scanner vasco = new Scanner(System.in);
+ System.out.print("Enter the limit: ");
+ int limit = vasco.nextInt();
+ int first = 0 ,second =1;
+ System.out.println("Fibonacci sequence up to " + limit + ":");
+
+ for(int i=1;i<=limit;i++)
+ {
+ System.out.print(first+" ");
+ int next = first + second;
+ first = second;
+ second = next;
+ }
+ 
+ }
+}
